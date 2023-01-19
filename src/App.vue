1bf24bc2e47b580b4c8519b173b84970b189e034
Daewon Park <30211069@edu.hanbat.ac.kr>
2023-01-19 16:27:50 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ message.split("").reverse().join("") }}</h1>
+  <p>숫자 세기: {{ counter.count }}</p>
+  <button @click='counter.count++'>증가</button>
+  <h1>{{ value }}</h1>
+  <button @click='value += "a"'>증가</button>
+</template>
+
+<script setup>
+import { reactive, ref } from "vue";
+
+let value = "hello";
+const counter = reactive({
+  count: 0,
+});
+const message = ref("안녕 Vue!");
+message.value = "안녕하세요";
+</script>
+
+<style lang='scss' scoped>
+
+</style>
